4aed5a658591f01730ec2d5ff4f98f82c827330f
Sayantan Hore <sayantanhore@Sayantans-MacBook-Pro.local>
2015-10-26 18:52:53 +0200

    Successfully read colour values on CoAP server

--- server.js
+++ server.js
@@ -9,7 +9,18 @@
 	
 	server.on('request', function(req, res){
 		console.log("Request received");
-		console.log(req.method)
+		console.log(JSON.stringify(req, null, 4))
+		
+		var buf = req['_packet']['payload'];
+		console.log("Buffer type :: " + typeof buf);
+		buf = JSON.parse(JSON.stringify(buf));
+		console.log("Buffer contains :: " + buf);
+		var colour_r = buf['data'][0];
+		var colour_g = buf['data'][1];
+		var colour_b = buf['data'][2];
+		console.log("Colour R :: " + colour_r);
+		console.log("Colour G :: " + colour_g);
+		console.log("Colour B :: " + colour_b);
 		res.end("Request received");
 	});
 	server.listen(function(){
