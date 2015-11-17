2b052db147d7ccf8f8992d62eadc73176d5197e4
Teemu Hakala <temmi@iki.fi>
2015-11-17 22:49:15 +0200

    total refactoring of the integration test suite
    still a shell script, now with functions that get called
    and most of the data is in a human readable format
    
    currently trying to figure out how to use indirectly referenced
    variables in a way that allows for reporting the test statuses

--- integration_test.sh
+++ integration_test.sh
@@ -1,17 +1,52 @@
 #!/bin/sh
 
-while true; do
-    colour=""
+sleep_period=2
 
-    for i in {1..3}; do
-    	col=$((RANDOM %= 255))
-    	colour=$colour$col" "
-	done
-
-    printf "%s" "$colour"
-    printf "\n\n"
-
+send_coap_colour () {
+    echo $colour
     echo $colour | node client.js
-    sleep 5
-done
+    eval test_${testname}_exec=$?
+}
 
+ask_user_success_status () {
+    cat <<EOT
+    The light should be coloured at $1
+    if it is, please press enter
+    
+    In case of any failure, type anything and press enter
+EOT
+    read string
+    eval test_${testname}_human=$?
+}
+
+light_test () {
+    colour=$(printf "%03d %03d %03d\n" $1 $2 $3)
+    testname=$4
+    send_coap_colour $colour $testname
+    ask_user_success_status $colour $testname
+    sleep $sleep_period
+}
+
+report () {
+    testname=$1
+    eval exec=test_${testname}_exec
+    eval human=test_${testname}_human
+    cat <<EOT
+test $testname
+  return code: $test_${testname}_exec
+  human evaluation: $test_${testname}_human
+EOT
+}
+
+light_test 255 255 255 first
+light_test 255 050 100 second
+light_test 255 000 000 third
+light_test 000 255 000 fourth
+light_test 000 000 255 fifth
+
+send_coap_colour "255 050 100" puppu
+
+for i in first second third fourth fifth
+do
+    report $i
+done
