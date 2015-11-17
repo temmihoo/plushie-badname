8f2d429715b6ff14410ce94b5597b10428ff861f
Sayantan Hore <sayantanhore@Sayantans-MacBook-Pro.local>
2015-11-17 18:28:35 +0200

    integration_test.sh changed to adjust indentation

--- integration_test.sh
+++ integration_test.sh
@@ -2,13 +2,15 @@
 
 while true; do
     colour=""
-    for i in {1..3};
-        do
-            col=$((RANDOM %= 255))
-            colour=$colour$col" "
-        done
+
+    for i in {1..3}; do
+    	col=$((RANDOM %= 255))
+    	colour=$colour$col" "
+	done
+
     printf "%s" "$colour"
     printf "\n\n"
+
     echo $colour | node client.js
     sleep 5
 done
