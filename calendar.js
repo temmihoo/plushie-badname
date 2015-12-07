4592bb811fc8a98668b3d538704028871f21dbca
Sayantan Hore <sayantanhore@Sayantans-MacBook-Pro.local>
2015-12-07 21:24:26 +0200

    Events added to calendar.js

--- calendar.js
+++ calendar.js
@@ -12,11 +12,11 @@
 		console.log("-------------------------------------\n");
 	}
 	
+	var checkInterval = 5000;
 	
 	var moment = require('moment');
 	
-	function getLocaleData(){
-		var now = moment();
+	function getLocaleData(now){
 	
 		var locale = {
 			RightNow: now.format('llll'),
@@ -30,10 +30,47 @@
 		return locale;
 	}
 	
-	setInterval(function(){
-		showComponents(getLocaleData());
-	}, 1000);
+	function checkIfEventHasOccurred(now, recordedMoment){
+		if (now.second() !== recordedMoment.second()){
+			return false;
+		}
+		else if(now.minute() !== recordedMoment.minute()){
+			return false;
+		}
+		else if(now.hour() !== recordedMoment.hour()){
+			return false;
+		}
+		else if(now.day() !== recordedMoment.day()){
+			return false;
+		}
+		else if(now.month() !== recordedMoment.month()){
+			return false;
+		}
+		else if(now.year() !== recordedMoment.year()){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
 	
+	var now = moment();
+	showComponents(getLocaleData(now));
+	
+	var intervals = [10, 15, 20];
+	
+	var events = intervals.map(function(value){
+		return now.clone().add(value.toString(), 'seconds'); 
+	});
+	
+	events.forEach(function(element, index){
+		setInterval(function(){
+			if (checkIfEventHasOccurred(moment(), element)){
+				showComponents(getLocaleData(element));
+			}
+		}, checkInterval);
+	});
+
 	//var CoapClient = require('./CoapClient');
     //CoapClient.command();
 })();
