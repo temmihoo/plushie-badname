bc525b591a40b775489302652c22b689032a9f6c
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-02-16 17:56:17 +0200

    'Event.js' modified to generate a random number of events

--- Event.js
+++ Event.js
@@ -4,6 +4,7 @@ module.exports.emitEvents = function(now, maxDelay){
     'use strict';
     
     var events = [];
+    var no_of_events = 0;
     
     function generate_random_string(){
         return Math.random().toString(36).slice(2); 
@@ -20,51 +21,26 @@ module.exports.emitEvents = function(now, maxDelay){
         return startAfter;
     }
     
-    var startAfter = setStartTimeInSecondsFromNow(5);
+    no_of_events = Math.floor(Math.random() * 10 + 1);
     
-    var startTime = now.clone().add(startAfter.toString(), 'seconds');
-    var endTime = now.clone().add((startAfter + 2).toString(), 'seconds');
-    
-    var event = {
-        id: generate_random_string(),
-        begin: {
-            time: startTime,
-            perform: function(){
-                return " started";
+    for (var i = 0; i < no_of_events; i ++){
+        var startAfter = setStartTimeInSecondsFromNow(5);
+        var event = {
+            id: generate_random_string(),
+            begin: {
+                time: now.clone().add(startAfter.toString(), 'seconds'),
+                perform: function(){
+                    console.log("Event starts");
+                }
+            },
+            end: {
+                time: now.clone().add((startAfter + 2).toString(), 'seconds'),
+                perform: function(){
+                    console.log("Event ends");
+                }
             }
-        },
-        end: {
-            time: endTime,
-            perform: function(){
-                return " ended";
-            }
-        }
-    };
-    
-    events.push(event);
-    
-    var startAfter = setStartTimeInSecondsFromNow(6);
-    
-    var startTime = now.clone().add(startAfter.toString(), 'seconds');
-    var endTime = now.clone().add((startAfter + 2).toString(), 'seconds');
-    
-    var event = {
-        id: generate_random_string(),
-        begin: {
-            time: startTime,
-            perform: function(){
-                return " started";
-            }
-        },
-        end: {
-            time: endTime,
-            perform: function(){
-                return " ended";
-            }
-        }
-    };
-    
-    events.push(event);
-    
+        };
+        events.push(event);
+    }
     return events;
 }
