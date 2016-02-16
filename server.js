381e2c3293b144a8b17c93f95c98dd51fa6ab701
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-02-16 17:58:16 +0200

    'server.js' modified to start calendar along with server and stop it when SIGINT received

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 // Server
 
+var pubsub = require('./PubSub');
+
 (function(){
     'use strict';
 	
     var clear = require('clear');
 	clear();
-	
-    var Util = require('./Util');
-    var util = Util.util();
+    
     var fs = require('fs');
+    var ps = require("./PubSub").pubsub();
+    
+    var CoapServer = require('./CoapServer');
+    var calendar = require('./Calendar');
+    var cal = calendar.getCalendar();
     
     process.on('SIGINT', function(){
         console.log("SIGINT received, process " + process.pid + " terminating ...\n");
         fs.unlinkSync("./temp/pid.txt");
+        cal.stop(ps);
         process.exit(0);
     });
     
     fs.writeFileSync("./temp/pid.txt", process.pid, 'utf8');
     
-    var CoapServer = require('./CoapServer');
     CoapServer.serv();
+    cal.start(ps);
 })();
